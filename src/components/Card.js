73d36651bc62b1d4d7cafe85362208ca55064414
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-09 12:43:18 -0500

    added pokeball placeholder image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { getLeadingZeros, getTypesArr }from "../constants/functions.js"
+import React, { useState, useEffect } from "react";
+import { getLeadingZeros, getTypesArr } from "../constants/functions.js";
 
 const Card = (obj) => {
-    const [pokemonDetails, setPokemonDetails] = useState([]);
+    const [pokemonDetails, setPokemonDetails] = useState({});
 
     useEffect(() => {
         const pokemon = obj.obj;
@@ -15,9 +15,9 @@ const Card = (obj) => {
                 let pokemonObj = {
                     id: data.id,
                     formattedId: getLeadingZeros(data.id),
-                    name: (data.name).toUpperCase(),
+                    name: data.name.toUpperCase(),
                     types: getTypesArr(data.types),
-                }
+                };
                 setPokemonDetails(pokemonObj);
             })
             .catch((err) => {
@@ -29,19 +29,23 @@ const Card = (obj) => {
         <a href={`${pokemonDetails.id}`} className="card grow">
             <img
                 className="card__image"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`}
+                src={
+                    pokemonDetails.id
+                        ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`
+                        : "/pokeball.png"
+                }
                 alt=""
                 width="160px"
                 height="160px"
             />
             <br />
-            <p className="card__title text--primary">{(pokemonDetails.name)}</p>
-            <p className="card__subtitle text--primary">#{pokemonDetails.formattedId}</p>
-            <div className="card__types">
-                {pokemonDetails.types}
-            </div>
+            <p className="card__title text--primary">{pokemonDetails.name}</p>
+            <p className="card__subtitle text--primary">
+                #{pokemonDetails.formattedId}
+            </p>
+            <div className="card__types">{pokemonDetails.types}</div>
         </a>
     );
-}
+};
 
 export default Card;
