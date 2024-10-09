8d89bc385f5e780de758b15e3a380cedd6d346d3
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-09 12:54:50 -0500

    added pokeball placeholder to details page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getLeadingZeros, getTypesArr }from "../constants/functions.js"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getLeadingZeros, getTypesArr } from "../constants/functions.js";
 
 // # fetch data
 // pokemon_data = requests.get(f"https://pokeapi.co/api/v2/pokemon/{number}/").json()
@@ -8,8 +8,8 @@ import { getLeadingZeros, getTypesArr }from "../constants/functions.js"
 // pokemon_evolution_data = requests.get(pokemon_species_data["evolution_chain"]["url"]).json()
 
 const DetailsPage = () => {
-    const { number } = useParams()
-    const [pokemonDetails, setPokemonDetails] = useState([]);
+    const { number } = useParams();
+    const [pokemonDetails, setPokemonDetails] = useState({});
 
     useEffect(() => {
         const url = "https://pokeapi.co/api/v2/pokemon/" + number;
@@ -22,20 +22,20 @@ const DetailsPage = () => {
                 let pokemonObj = {
                     id: data.id,
                     formattedId: getLeadingZeros(data.id),
-                    name: (data.name).toUpperCase(),
+                    name: data.name.toUpperCase(),
                     types: getTypesArr(data.types),
                     height: data.height / 10,
                     weight: data.weight / 10,
-                    nextId: ((data.id === 150) ? 1 : data.id + 1),
-                    prevId: ((data.id === 1) ? 150 : data.id - 1)
-                }
+                    nextId: data.id === 150 ? 1 : data.id + 1,
+                    prevId: data.id === 1 ? 150 : data.id - 1,
+                };
                 setPokemonDetails(pokemonObj);
             })
             .catch((err) => {
                 console.log("pokemon not found", err);
             });
     }, []);
-    
+
     return (
         <div className="flex-container">
             <div className="hero">
@@ -46,18 +46,31 @@ const DetailsPage = () => {
                     <p>HEIGHT: {pokemonDetails.height} METERS</p>
                     <p>WEIGHT: {pokemonDetails.weight} KILOGRAMS</p>
                 </div>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`}>
-                </img>
+                <img
+                    className="hero__img"
+                    src={
+                        pokemonDetails.id
+                            ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`
+                            : "/pokeball.png"
+                    }
+                />
             </div>
 
             <div className="nav">
-                <a className="nav__link" href={pokemonDetails.prevId}>{`PREV`}</a>
-                <a className="nav__link" href="/">HOME</a>
-                <a className="nav__link" href={pokemonDetails.nextId}>{`NEXT`}</a>
+                <a
+                    className="nav__link"
+                    href={pokemonDetails.prevId}
+                >{`PREV`}</a>
+                <a className="nav__link" href="/">
+                    HOME
+                </a>
+                <a
+                    className="nav__link"
+                    href={pokemonDetails.nextId}
+                >{`NEXT`}</a>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default DetailsPage;
